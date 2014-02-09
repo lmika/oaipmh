75435cc07abbe5102ebdc20fb1a101b3b54e184f
Leon Mika <l.mika@bom.gov.au>
2014-02-10 10:58:08 +1100

    Small format change to long set listings.

--- src/commands.go
+++ src/commands.go
@@ -39,12 +39,12 @@ func (lc *SetsCommand) Flags(fs *flag.FlagSet) *flag.FlagSet {
 func (lc *SetsCommand) Run(args []string) {
     lc.Ctx.Session.ListSets(0, -1, func(res ListSetResult) bool {
         if (*(lc.flagDetailed)) {
-            fmt.Printf("Name: %s\nSpec: %s\n", res.Spec, res.Name)
+            fmt.Printf("Name: %s\nSpec: %s\n\n", res.Spec, res.Name)
             descrLines := strings.Split(res.Description, "\n")
             for _, v := range descrLines {
                 v = strings.Trim(v, " \n\t")
                 if (v != "") {
-                    fmt.Printf("    %s\n", v)
+                    fmt.Printf("%s\n", v)
                 }
             }
             fmt.Println("---")
