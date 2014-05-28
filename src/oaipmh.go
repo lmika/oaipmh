0bdd1bb93f77151b9cdf1e08993fda87342f6d4b
Leon Mika <l.mika@bom.gov.au>
2014-05-28 10:26:02 +1000

    Updated version number

--- src/oaipmh.go
+++ src/oaipmh.go
@@ -15,7 +15,7 @@ import (
 )
 
 const APP_NAME string = "oaipmh-viewer"
-const APP_VERSION string = "0.1"
+const APP_VERSION string = "1.1"
 
 // Flags
 var prefix *string = flag.String("p", "iso19139", "The record prefix to retrieve")
